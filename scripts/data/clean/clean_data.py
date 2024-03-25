c52af9292e2ffae03d1e840937752ae2c24de2d6
Perrine <perrine@PetXs-MacBook-Pro.local>
2024-03-25 20:49:02 +0100

    feat: ajout scripts clean_data.py et extract_data.py

---
+++ scripts/data/clean/clean_data.py
@@ -0,0 +1,24 @@
+import re
+import os
+
+
+def clean_text(text):
+    text = re.sub(r"\[\d+\]", "", text)
+    text = re.sub(r"\D\.\d+", ".", text)
+    text = re.sub(r"(?<=\D)\d+[,]?", "", text).strip()
+
+    return text
+
+def clean_data(data_clean, data_raw):
+    for raw_file in os.listdir(data_raw):
+        with open(data_raw+raw_file, 'r', encoding='utf-8') as f:
+            text = f.read()
+            text = clean_text(text)
+            with open(data_clean + raw_file, 'w', encoding='utf-8') as f:
+                f.write(text)
+
+
+data_raw = "./data/raw/"
+data_clean = "./data/clean/"
+
+clean_data(data_clean, data_raw)
---
+++ scripts/data/raw/extract_data.py
@@ -0,0 +1,89 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import os
+import urllib.parse
+
+def get_urls(driver, base_url):
+    driver.get(base_url)
+    # Attendre que les liens interlangues soient chargés
+    wait = WebDriverWait(driver, 10)
+    interlanguage_links = wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, 'li.interlanguage-link a')))
+
+    # Récupérer les URLs de tous les liens interlangues disponibles sur la page
+    urls = {link.get_attribute('lang'): link.get_attribute('href') for link in interlanguage_links}
+    # print(urls)
+    return urls
+
+def extract_first_paragraph(driver, url):
+    driver.get(url)
+    
+    paragraph = None
+
+    # Extrait le premier paragraphe
+    try:
+        elements = driver.find_elements(By.XPATH, '//*[@id="mw-content-text"]/div/p')
+        if elements:
+            if elements[0] == "":
+                paragraph = elements[1].text + "\n" + elements[2].text
+            else:
+                paragraph = elements[0].text + "\n" + elements[1].text
+    except:
+        print(f"Could not find paragraph for {url}")
+
+    return paragraph
+
+
+def write_txt(data_folder, lang, url, paragraph):
+    # Parse the URL to extract the article title, which is the part after the last '/'
+    parsed_url = urllib.parse.urlparse(url)
+    article_title = os.path.basename(parsed_url.path)
+    
+    # URL decode the article title (e.g., replace '%E1%83%90' with actual characters)
+    article_title = urllib.parse.unquote(article_title)
+    
+    # Replace any remaining invalid file name characters (like '/') in the title with '_'
+    valid_article_title = ''.join(char if char.isalnum() else '_' for char in article_title)
+    
+    # Ensure the data folder exists
+    os.makedirs(data_folder, exist_ok=True)
+    
+    # Generate the filename and write the paragraph to the file
+    filename = f"{valid_article_title}_{lang}.txt"
+    file_path = os.path.join(data_folder, filename)
+    
+    with open(file_path, 'w', encoding='utf-8') as f:
+        f.write(paragraph)
+
+
+def main():
+    # options = webdriver.ChromeOptions()
+    # options.add_experimental_option('detach', True)
+
+    driver = webdriver.Chrome()
+    
+    data_folder = "./data/raw/"
+
+    base_urls = ['https://ar.wikipedia.org/wiki/شجرة', 'https://fr.wikipedia.org/wiki/Biochimie', 'https://en.wikipedia.org/wiki/Natural_language_processing']
+    languages = ['ar', 'fr', 'en', 'de', 'es', 'ja', 'ko', 'ru', 'zh']
+    
+    # Boucle sur les URLs de base
+    for base_url in base_urls:
+        urls = get_urls(driver, base_url)
+        
+        # Filter the URLs for the desired languages
+        filtered_urls = {lang: urls[lang] for lang in languages if lang in urls}
+        
+        for lang, url in filtered_urls.items():
+            paragraph = extract_first_paragraph(driver, url)
+            print(f"Langue: {lang}\nParagraph: {paragraph}\n")
+            
+            # Write the results to text files
+            write_txt(data_folder, lang, url, paragraph)
+
+
+    driver.quit()
+
+if __name__ == "__main__":
+    main()
